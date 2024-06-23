cabccad0590c879e2cd27156a400e6a2a34911ea
Nazaga <nazaga.py@gmail.com>
2024-06-23 15:33:15 +0400

    Adding functionalitys like changing theme, editing, moving and opening card view for cards

--- flashcard_flask.py
+++ flashcard_flask.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask import render_template,redirect,url_for,request
+from flask import render_template,redirect,url_for,request,Response
 import pickle
 import json
 from collections import OrderedDict
 import os
 import webbrowser
 import pyautogui
+
 app = Flask(__name__)
 
 class Card:
@@ -17,6 +18,9 @@ class Card:
       self.level = level
       cards.add_card(self)
       cards.update_collection()
+
+   def update_card_id(self):
+      self.card_id = (self.theme+self.front).encode().hex()
 
 class CardCollection(OrderedDict):
    def add_card(self,card):
@@ -36,23 +40,25 @@ class CardCollection(OrderedDict):
       self.update_collection()
 
    def rename_theme(self,theme,new_theme):
-      for card in self[theme]:
-         card.theme = new_theme
       self[new_theme] = self.pop(theme)
+      for card in list(self[new_theme].values()):
+         card.theme = new_theme
+         card.update_card_id()
       self.update_collection()
 
-   def move_card(self,card,new_theme):
-      self[new_theme] = self[card.theme].pop(card.card_id)
+   def move_card(self,old_theme,card_id,new_theme):
+      card = self[old_theme].pop(card_id)
+      Card(new_theme, card.front, card.back, card.level)
       self.update_collection()
-
 
    def get_levels(self,theme):
       levels = {}
       for t,c in list(self.items()):
-         levels[t] = OrderedDict([(0,list(self[t].values())),(1,[]),(2,[]),(3,[])])
+         levels[t] = OrderedDict([(0,list(self[t].values()))])
          levels[t][0] = list(self[t].values())
          for c_id,cc in list(self[t].items()):
             levels[t].setdefault(cc.level,[]).append(cc)
+         levels[t] = OrderedDict(sorted(levels[t].items()))
       return levels[theme]
 
    def update_collection(self):
@@ -78,7 +84,11 @@ def new_theme():
       return render_template('new_theme.html')
    elif request.method == 'POST':
       new_theme = request.form.get('new_theme')
-      if new_theme and new_theme not in list(cards.keys()):
+      changed_theme = request.form.get('changed_theme')
+      cards_list = list(cards.keys())
+      if (new_theme in cards_list) and (changed_theme not in cards_list) and changed_theme:
+         cards.rename_theme(new_theme,changed_theme)
+      elif new_theme and new_theme not in cards_list:
          cards.add_theme(new_theme)
       return redirect('/')
 
@@ -90,11 +100,32 @@ def theme(theme):
       cards.del_theme(theme)
       return redirect('/')
 
+@app.route('/card_view/<theme>/<card_id>',methods=["GET"])
+def card_view(theme,card_id):
+   if request.method == "GET":
+      return render_template('card_view.html',card=cards[theme][card_id])
+
 @app.route('/<theme>/<card_id>',methods=["POST"])
 def del_card(theme,card_id):
    if request.method == "POST":
       cards.del_card(cards[theme][card_id])
       return redirect(url_for('theme',theme=theme))
+
+@app.route('/move_card/<old_theme>/<card_id>',methods=["GET","POST"])
+def move_card(old_theme,card_id):
+   if request.method == "GET":
+      print(cards)
+      return render_template('move_card.html',
+         content=dict(old_theme=old_theme,
+            card_id=card_id,
+            themes=list(cards.keys())
+            )
+         )
+   elif request.method == "POST":
+      to_theme = request.form.get("to_theme")
+      cards.move_card(old_theme,card_id,to_theme)
+      print(cards)
+      return redirect('/')
 
 @app.route('/<theme>/new_card',methods=['GET','POST'])
 def new_card(theme):
@@ -105,6 +136,20 @@ def new_card(theme):
       if new_card:
          Card(theme,new_card.get('front'),new_card.get('back'),int(new_card.get('level','1')))
       return redirect(url_for('theme',theme=theme))
+
+@app.route('/edit_card/<referrer>/<theme>/<card_id>',methods=["GET","POST"])
+def edit_card(referrer,theme,card_id):
+   if request.method == "GET":
+      return render_template('edit_card.html',
+         content={'card':cards[theme][card_id],'referrer':referrer})
+   elif request.method == "POST":
+      card_form = request.form
+      cards[theme].pop(card_id)
+      Card(theme,card_form.get('front'),card_form.get('back'),int(card_form.get('level')))
+      if referrer == "theme":
+         return redirect(f'/{theme}')
+      else:
+         return redirect(f'/practice/{theme}/{referrer}')
 
 @app.route('/practice/<theme>/<int:le>',methods=["GET","POST"])
 def practice(theme,le):
